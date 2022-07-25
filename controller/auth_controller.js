c2a5e67ee7e3638b77b7a661d3d8f6090b3bf4dc
Anirban-1490 <anirbanpratihar45@gmail.com>
2022-07-25 23:55:16 +0530

    dd

--- controller/auth_controller.js
+++ controller/auth_controller.js
@@ -61,7 +61,7 @@ const newUser_handler = async(req,res,next)=>{
 
 const authorizeUser = async (req,res)=>{
 
-    console.log(req.secure);
+    console.log(res);
     //* get the token from client side
     const userToken = req.headers.authorization;
     if(!userToken || !userToken.startsWith("Bearer")){
