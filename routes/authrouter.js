9247996e6179ec34853bd7645e393db3c1c513f8
Anirban <anirbanpratihar45@gmail.com>
2023-02-17 17:40:51 +0530

    route for logged out handler

--- routes/authrouter.js
+++ routes/authrouter.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
+const {
+    login_handler,
+    newUser_handler,
+    authorizeUser,
+    logoutHandler,
+} = require("../controller/auth_controller");
 
-const {login_handler,newUser_handler,authorizeUser} = require("../controller/auth_controller")
-
-router.post("/signin",login_handler);
-router.post("/signup",newUser_handler);
-router.get("/authorize",authorizeUser)
-
-module.exports = router
+router.post("/signin", login_handler);
+router.post("/signup", newUser_handler);
+router.get("/authorize", authorizeUser);
+router.patch("/log-out", logoutHandler);
+module.exports = router;
