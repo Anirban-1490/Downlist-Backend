5c3c54feee20d11d99babc70226b2342ad160bca
Anirban <anirbanpratihar45@gmail.com>
2023-03-23 16:54:28 +0530

    route updated for adding a comment

--- routes/comment.js
+++ routes/comment.js
@@ -1,13 +1,13 @@
 const {
-  addComment,
-  fetchComment,
-  dislikeCommentHandler,
-  likeCommentHandler,
+    addComment,
+    fetchComment,
+    dislikeCommentHandler,
+    likeCommentHandler,
 } = require("../controller/comment_controller");
 
 const router = require("express").Router({ mergeParams: true });
 
-router.post("/user/:userID", addComment);
+router.post("/add", addComment);
 router.get("/list", fetchComment);
 router.put("/like", likeCommentHandler);
 router.put("/dislike", dislikeCommentHandler);
