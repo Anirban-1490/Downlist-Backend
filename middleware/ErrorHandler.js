2bb705af59005de5349372d64334dc43a3ef34ab
Anirban <anirbanpratihar45@gmail.com>
2023-03-23 16:53:38 +0530

    improved the codebase

--- middleware/ErrorHandler.js
+++ middleware/ErrorHandler.js
@@ -11,33 +11,22 @@ const errorHandler = (err, req, res, next) => {
         return validationError(err, res);
     }
 
-    if (err.name === "PinnedItemLimitError") {
-        return res.status(400).json({ message: err.message });
-    }
-    if (err.name === "EmptyUsername") {
+    const errorNames = [
+        "PinnedItemLimitError",
+        "EmptyUsername",
+        "EmptyEmail",
+        "EmptyPassword",
+        "NoAccount",
+        "InvalidUserID",
+    ];
+
+    if (errorNames.includes(err.name)) {
         return res.status(400).json({
             message: err.message,
-            name: "EmptyUsername",
+            name: err.name,
         });
     }
-    if (err.name === "EmptyEmail") {
-        return res.status(400).json({
-            message: err.message,
-            name: "EmptyEmail",
-        });
-    }
-    if (err.name === "EmptyPassword") {
-        return res.status(400).json({
-            message: err.message,
-            name: "EmptyPassword",
-        });
-    }
-    if (err.name === "NoAccount") {
-        return res.status(400).json({
-            message: err.message,
-            name: "NoAccount",
-        });
-    }
+
     //? for other unknown errors
     else {
         return res.status(500).send({ message: "an unknown error occurred" });
